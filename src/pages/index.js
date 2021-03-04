bd21b8b8b63387d156d1d9d65b3c367f4e403dff
Kylan Wong <mynameiskylan@gmail.com>
2021-03-04 12:21:19 -0800

    add profile image

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,21 +7,28 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const Button = styled(Link)`
-  border-radius:5px;
-  background: rgba(0,0,0,0.8);
-  color:white;
-  padding:1em;
-  transition:0.2s ease;
+  border-radius: 5px;
+  background: rgba(0, 0, 0, 0.8);
+  color: white;
+  padding: 1em;
+  transition: 0.2s ease;
 
   &:hover {
-    opacity:0.9;
+    opacity: 0.9;
   }
 `
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div className='container'>
+    <div className="container">
+      <StaticImage
+        src={"../images/kylan(1).jpg"}
+        alt={"Me!"}
+        id="profile-image"
+        style={{ width: 60 + "%", overflow: "visible" }}
+        aspectRatio={1}
+      />
       <h2>Hi. My name is Kylan, and I'm a full-stack web developer.</h2>
       <p>
         <Button to="/projects">My Portfolio</Button> <br />
